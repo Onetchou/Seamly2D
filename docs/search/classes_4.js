c72bbcbf5140993708ed7e035fc9c8839d750cb3
Onetchou <Onetchou@users.noreply.github.com>
2025-01-06 02:40:33 +0000

    Deploying to gh-pages from @ Onetchou/Seamly2D@fe07a432de96a4f1019045a71522d4cf2945d313 🚀

--- docs/search/classes_4.js
+++ docs/search/classes_4.js
@@ -1,9 +1,9 @@
 var searchData=
 [
-  ['editformuladialog_9700',['EditFormulaDialog',['../d1/d7d/classEditFormulaDialog.html',1,'']]],
-  ['editgroupdialog_9701',['EditGroupDialog',['../d3/da7/classEditGroupDialog.html',1,'']]],
-  ['editlabeltemplatedialog_9702',['EditLabelTemplateDialog',['../d7/df1/classEditLabelTemplateDialog.html',1,'']]],
-  ['expandingtextedit_9703',['ExpandingTextEdit',['../db/dac/classExpandingTextEdit.html',1,'']]],
-  ['exportformatcombobox_9704',['ExportFormatCombobox',['../d4/d06/classExportFormatCombobox.html',1,'']]],
-  ['exportlayoutdialog_9705',['ExportLayoutDialog',['../de/dbd/classExportLayoutDialog.html',1,'']]]
+  ['editformuladialog_0',['EditFormulaDialog',['../d1/d7d/classEditFormulaDialog.html',1,'']]],
+  ['editgroupdialog_1',['EditGroupDialog',['../d3/da7/classEditGroupDialog.html',1,'']]],
+  ['editlabeltemplatedialog_2',['EditLabelTemplateDialog',['../d7/df1/classEditLabelTemplateDialog.html',1,'']]],
+  ['expandingtextedit_3',['ExpandingTextEdit',['../db/dac/classExpandingTextEdit.html',1,'']]],
+  ['exportformatcombobox_4',['ExportFormatCombobox',['../d4/d06/classExportFormatCombobox.html',1,'']]],
+  ['exportlayoutdialog_5',['ExportLayoutDialog',['../de/dbd/classExportLayoutDialog.html',1,'']]]
 ];
--- docs/search/classes_9.js
+++ docs/search/classes_9.js
@@ -1,9 +1,9 @@
 var searchData=
 [
-  ['layoutsettingsdialog_9725',['LayoutSettingsDialog',['../db/d32/classLayoutSettingsDialog.html',1,'']]],
-  ['linetypecombobox_9726',['LineTypeComboBox',['../d9/de2/classLineTypeComboBox.html',1,'']]],
-  ['linetypeproperty_9727',['LineTypeProperty',['../d5/daa/classVPE_1_1LineTypeProperty.html',1,'VPE']]],
-  ['lineweightcombobox_9728',['LineWeightComboBox',['../d6/d43/classLineWeightComboBox.html',1,'']]],
-  ['lineweightproperty_9729',['LineWeightProperty',['../d8/ddf/classVPE_1_1LineWeightProperty.html',1,'VPE']]],
-  ['liteparseevent_9730',['LiteParseEvent',['../d5/d08/classLiteParseEvent.html',1,'']]]
+  ['layoutsettingsdialog_0',['LayoutSettingsDialog',['../db/d32/classLayoutSettingsDialog.html',1,'']]],
+  ['linetypecombobox_1',['LineTypeComboBox',['../d9/de2/classLineTypeComboBox.html',1,'']]],
+  ['linetypeproperty_2',['LineTypeProperty',['../d5/daa/classVPE_1_1LineTypeProperty.html',1,'VPE']]],
+  ['lineweightcombobox_3',['LineWeightComboBox',['../d6/d43/classLineWeightComboBox.html',1,'']]],
+  ['lineweightproperty_4',['LineWeightProperty',['../d8/ddf/classVPE_1_1LineWeightProperty.html',1,'VPE']]],
+  ['liteparseevent_5',['LiteParseEvent',['../d5/d08/classLiteParseEvent.html',1,'']]]
 ];
